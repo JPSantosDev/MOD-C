db793a86f208c182a0a00005886e64e29795735f
Jean Pierre <jp0056392@gmail.com>
2026-07-26 22:58:43 -0300

    mudança no field de pin

--- app/src/main/java/com/example/mod_c/ui/components/AppComponents.kt
+++ app/src/main/java/com/example/mod_c/ui/components/AppComponents.kt
@@ -1,11 +1,13 @@
 package com.example.mod_c.ui.components
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -14,46 +16,44 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun PinField(){
-    val pin = remember { mutableStateListOf("","","","") }
-    val focusRequesters = List(4) { FocusRequester() }
+    var pin by remember { mutableStateOf("") }
 
 
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        repeat(4) { index ->
-            Box(
-                modifier = Modifier
-                    .size(48.dp)
-                    .focusRequester(focusRequesters[index])
-            ){
-                TextField(
-                    value = pin[index],
-                    onValueChange = { value->
-
-                            pin[index] = value.takeLast(1)
-
-                            if (pin[index].isNotEmpty() && index < 3){
-                                focusRequesters[index+1].requestFocus()
-                            }
-                             else if(pin[index].isEmpty() && index>0){
-                                focusRequesters[index-1].requestFocus()
-                            }
-
+    BasicTextField(
+        value = pin,
+        onValueChange = {
+            if (pin.length <=4 && pin.all { it.isDigit() }) pin=it
+        },
+        decorationBox = {
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                repeat(4) { index ->
+                    Box(
+                        modifier = Modifier
+                            .size(48.dp)
+                            .border(1.dp,Color.Gray),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Text(
+                            text = pin.getOrNull(index)?.toString() ?: ""
+                        )
                     }
-                )
+                }
             }
         }
-    }
+    )
+
 }
 
 @Preview
